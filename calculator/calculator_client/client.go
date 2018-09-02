911c0f96f56931d7148f4a1e3054b59ce723cf0d
Takahiko Okada <takahikookada@gmail.com>
2018-09-02 21:25:27 +0200

    client side config done

---
+++ calculator/calculator_client/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/tkhkokd/grpc/calculator/calculatorpb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello I'm a Calculator client")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := calculatorpb.NewCalculatorServiceClient(cc)
+	// fmt.Printf("Created client: %f", c)
+	doUnary(c)
+}
+
+func doUnary(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do a Sum Unary RPC...")
+	req := &calculatorpb.SumRequest{
+		FirstNumber:  5,
+		SecondNumber: 10,
+	}
+
+	res, err := c.Sum(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Sum RPC: %v", err)
+	}
+	log.Printf("Response from Sum: %v", res.SumResult)
+}
